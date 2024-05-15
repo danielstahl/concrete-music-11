1ea11199800a8f621825afb7f4fa2120e0cb0006
Daniel Ståhl <oe.daniel.stahl@gmail.com>
2024-05-15 20:13:11 +0200

    First mix

--- reaperCreateScript.lua
+++ reaperCreateScript.lua
@@ -23,41 +23,28 @@ function ImportAudio(index, channel, trackName, fileName, lastInFolder)
   reaper.SetMediaItemTakeInfo_Value(take, "I_CHANMODE", channel + 64 + 2)
 end
 
-audioFile = "/Users/danielstahl/Documents/Music/Pieces/Concrete Music/Concrete Music 11/stage/concreteMusic11Score-v1.caf"
+audioFile = "/Users/danielstahl/Documents/Music/Pieces/Concrete Music/Concrete Music 11/stage/concreteMusci11Score.caf"
 
-CreateFolder(0, "Low Fm 1")
-ImportAudio(1, 1, "Low Fm 1 Clean", audioFile, false)
-ImportAudio(2, 3, "Low Fm 1 Effect", audioFile, true)
+CreateFolder(0, "Low pothit scratch")
+ImportAudio(1, 1, "Low pothit scratch Effect", audioFile, false)
+ImportAudio(2, 3, "Low pothit scratch Clean", audioFile, true)
 
-CreateFolder(3, "Middle Fm 1")
-ImportAudio(4, 5, "Middle Fm 1 Clean", audioFile, false)
-ImportAudio(5, 7, "Middle Fm 1 Effect", audioFile, true)
+CreateFolder(3, "Middle pothit scratch")
+ImportAudio(4, 5, "Middle pothit scratch Effect", audioFile, false)
+ImportAudio(5, 7, "Middle pothit scratch Clean", audioFile, true)
 
-CreateFolder(6, "High Fm 1")
-ImportAudio(7, 9, "High Fm 1 Clean", audioFile, false)
-ImportAudio(8, 11, "High Fm 1 Effect", audioFile, true)
+CreateFolder(6, "High pothit scratch")
+ImportAudio(7, 9, "High pothit scratch Effect", audioFile, false)
+ImportAudio(8, 11, "High pothit scratch Clean", audioFile, true)
 
+CreateFolder(9, "Low short pot hit rattle")
+ImportAudio(10, 13, "Low short pot hit rattle Effect", audioFile, false)
+ImportAudio(11, 15, "Low short pot hit rattle Clean", audioFile, true)
 
-CreateFolder(9, "Low Fm 2")
-ImportAudio(10, 13, "Low Fm 2 Clean", audioFile, false)
-ImportAudio(11, 15, "Low Fm 2 Effect", audioFile, true)
+CreateFolder(12, "Middle short pot hit rattle")
+ImportAudio(13, 17, "Middle short pot hit rattle Effect", audioFile, false)
+ImportAudio(14, 19, "Middle short pot hit rattle Clean", audioFile, true)
 
-CreateFolder(12, "Middle Fm 2")
-ImportAudio(13, 17, "Middle Fm 2 Clean", audioFile, false)
-ImportAudio(14, 19, "Middle Fm 2 Effect", audioFile, true)
-
-CreateFolder(15, "High Fm 2")
-ImportAudio(16, 21, "High Fm 2 Clean", audioFile, false)
-ImportAudio(17, 23, "High Fm 2 Effect", audioFile, true)
-
-CreateFolder(18, "Low Noise")
-ImportAudio(19, 25, "Low Noise Clean", audioFile, false)
-ImportAudio(20, 27, "Low Noise Effect", audioFile, true)
-
-CreateFolder(21, "Middle Fm 2")
-ImportAudio(22, 29, "Middle Noise Clean", audioFile, false)
-ImportAudio(23, 31, "Middle Noise Effect", audioFile, true)
-
-CreateFolder(24, "High Noise")
-ImportAudio(25, 33, "High Noise Clean", audioFile, false)
-ImportAudio(26, 35, "High Noise Effect", audioFile, true)
+CreateFolder(15, "High short pot hit rattle")
+ImportAudio(16, 21, "High short pot hit rattle Effect", audioFile, false)
+ImportAudio(17, 23, "High short pot hit rattle Clean", audioFile, true)
